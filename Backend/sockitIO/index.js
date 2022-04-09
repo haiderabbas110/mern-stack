18b8d9483ca03da53780d269a7a76442ea3087be
haider.abbas <haider.abbas@genetechsolutions.com>
2022-04-09 05:50:28 +0500

    socket io class

---
+++ Backend/sockitIO/index.js
@@ -0,0 +1,11 @@
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+
+const httpServer = createServer();
+const io = new Server(httpServer, { /* options */ });
+
+io.on("connection", (socket) => {
+  // ...
+});
+
+httpServer.listen(4000);
