09cee392a711e2797b9b5635ce6aeacc932f8855
= <haidersayani95@gmail.com>
2022-04-09 07:45:46 +0500

    socket io chat app

--- Backend/sockitIO/index.js
+++ Backend/sockitIO/index.js
@@ -1,11 +1,25 @@
-const { createServer } = require("http");
-const { Server } = require("socket.io");
+const express = require('express');
 
-const httpServer = createServer();
-const io = new Server(httpServer, { /* options */ });
+const app = express();
+const port = 3009;
 
-io.on("connection", (socket) => {
-  // ...
+const http = require('http');
+const server = http.createServer(app)
+const {Server} = require('socket.io');
+const io = new Server(server);
+
+app.set('view engine','ejs');
+app.get('/chat',(req,res) => {
+  // res.send('Hello World');
+  res.render('chat/index')
+})
+
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+    socket.broadcast.emit('chat message', msg);
+  });
 });
 
-httpServer.listen(4000);
+server.listen(port,function(){
+  console.log(`Port is now running @ ${port}` )
+})
