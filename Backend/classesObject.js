cbc6890ec6fb0b84fc34c5f3086a2a218986fef4
= <haidersayani95@gmail.com>
2022-05-19 05:19:53 +0500

    class object and super

--- Backend/classesObject.js
+++ Backend/classesObject.js
@@ -1,34 +1,44 @@
-class Person{
-    constructor(name){
-        this.name = name;
-    }
-    //getter
-    get writeOnConsole(){
-        console.log(`hello from getter ${this.name}`)
-    }
-    set setOnConsole(message){
-        this.message = message;
+class Employee {
+    constructor(uname,email,salary,age,location){
 
-    }
-    sayName(){
-        console.log(`hi my name is ${this.name}`)
-    }
-}
+        this.uname = uname
+        this.email = email
+        this.salary = salary
+        this.location = location
+        this.age = age
 
-class Developer extends Person{
-    constructor(name){
-        super(name);
-        this.name = name;
     }
 
     getBio(){
-        super.sayName();
-        console.log("I am a developer")
+        console.log(`Hi, I am ${this.uname} and my email is ${this.email} my salary is ${salary} and location is ${this.location}`)
     }
 
-}
-let Boy = new Developer('haider bbas');
-Boy.writeOnConsole;
-Boy.setOnConsole = "hi kece ho data"
-Boy.getBio()
+    salaryInyear(val){
 
+        console.log(this.salary*val);
+
+        }
+
+}
+
+class EmployeeStatus extends Employee{
+    constructor(uname,email,salary,age,location,status){
+        super(uname,email,salary,age,location);
+        this.status = status
+    }
+
+
+    getStatus(){
+
+        console.log(`This is the satus of emp ${this.status }`);
+
+    }
+}
+
+
+
+let emp = new EmployeeStatus("ali","ali2@yopmial.com",20000,2,"karachi","Good");
+// let emp = new EmployeeStatus("good");
+console.log(emp);
+console.log(emp.getStatus());
+// console.log(`${emp} and ${emp.salaryInyear(200)}`);
