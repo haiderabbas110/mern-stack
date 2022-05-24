54c36daa5c38e2a77b368a35c6e80ad4d054c997
haider.abbas <haider.abbas@genetechsolutions.com>
2022-05-24 10:28:08 +0500

    breif explain classes object super getters and setters

--- Backend/classesObject.js
+++ Backend/classesObject.js
@@ -1,44 +1,57 @@
 class Employee {
-    constructor(uname,email,salary,age,location){
-
+    /* 
+    1) Constructor is used to initialize the state of an object. Method is used to expose behaviour of an object.
+    2) Constructor must not have return type. Method must have return type.
+    3) Constructor is invoked implicitly. Method is invoked explicitly.
+    4) Getters(), or accessors, are methods that provide access to an object's instance variables. Setters(), or mutators, are methods that provide the caller with an opportunity to update the value of a particular instance variable.
+*/
+    constructor(uname, email, salary, designation, location) {
         this.uname = uname
         this.email = email
         this.salary = salary
         this.location = location
-        this.age = age
-
+        this.designation = designation
     }
 
-    getBio(){
-        console.log(`Hi, I am ${this.uname} and my email is ${this.email} my salary is ${salary} and location is ${this.location}`)
+    info() {
+        console.log(`Hi, I am ${this.uname} and my designation is ${this.designation} and my email is ${this.email} my salary is ${this.salary} and location is ${this.location}`)
     }
-
-    salaryInyear(val){
-
-        console.log(this.salary*val);
-
-        }
 
 }
 
-class EmployeeStatus extends Employee{
-    constructor(uname,email,salary,age,location,status){
-        super(uname,email,salary,age,location);
-        this.status = status
+class Manager extends Employee {
+    /*  constructor(uname, email, salary, designation, location, status) {
+         super(uname, email, salary, designation, location);
+         this.status = status
+     } */
+
+    allownces() {
+        let fuel = 3000;
+        let accomodation = 10000;
+        let salary = this.salary + fuel + accomodation;
+        console.log(`Hi, I am ${this.uname} and my designation is ${this.designation} and my email is ${this.email} my salary is ${salary} and location is ${this.location}`)
     }
 
+    get role() {
+        return `${this.uname} is a ${this.designation}`
+    }
 
-    getStatus(){
-
-        console.log(`This is the satus of emp ${this.status }`);
-
+    set role(arg) {
+        /* if(arg.length < 1){
+            throw new Error("Data is empty")
+        } */
+        this.designation = arg
     }
 }
 
 
+let emp = new Employee("ali", "ali2@yopmial.com", 20000, "employee", "karachi");
+let manager = new Manager("ahmed", "ahmed@yopmial.com", 30000, "manager", "karachi");
+emp.info();
+manager.allownces()
 
-let emp = new EmployeeStatus("ali","ali2@yopmial.com",20000,2,"karachi","Good");
-// let emp = new EmployeeStatus("good");
-console.log(emp);
-console.log(emp.getStatus());
-// console.log(`${emp} and ${emp.salaryInyear(200)}`);
+//getter
+// console.log(manager.role)
+//setter
+manager.role = "Senior manager";
+console.log(manager.role);
