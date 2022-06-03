fa560b9d853fc5d7302be7524848a7079026bb06
haider.abbas <haider.abbas@genetechsolutions.com>
2022-06-03 18:20:52 +0500

    passport o otth facebook

--- Backend/passport-facebook.js
+++ Backend/passport-facebook.js
@@ -1,4 +1,4 @@
-import express from "express";
+/* import express from "express";
 import passport from 'passport';
 import session from "express-session";
 import facebookStrategy from 'passport-facebook'
@@ -8,55 +8,96 @@ const _facebookStrategy = facebookStrategy.Strategy;
 const app = express();
 
 app.use(passport.initialize());
-// app.use(passport.session());
-app.use(session({
-  secret:'thisissectret',
-  
-}))
 app.use(passport.session());
+app.use(session({
+  secret: 'thisissectret',
+  resave: false,
+  saveUninitialized: false
+
+}))
+//app.use(passport.session());
 
 passport.use(new _facebookStrategy({
   clientID: 551692553131393,
   clientSecret: '1f211708ce869676bbbe39e3295d48d8',
   callbackURL: "http://localhost:3000/auth/facebook/callback",
-  profileFields:['id','displayName','name','gender']
+  profileFields: ['id', 'displayName', 'name', 'gender']
 },
-function(accessToken, refreshToken, profile, done) {
-  console.log(profile);
-  return done(null,profile);
-}
+  function (accessToken, refreshToken, profile, done) {
+    console.log(profile);
+    return done(null, profile);
+  }
 ));
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
   res.send("hello");
 });
 
-app.get('/auth/facebook',passport.authenticate('facebook',{scope:'email'}));
+app.get('/auth/facebook', passport.authenticate('facebook', { scope: 'email' }));
 
-app.get('/facebook/callback',passport.authenticate('facebook',{
-  
-  successRedirect:"/profile",
-  failureRedirect:"/failed"
+app.get('/facebook/callback', passport.authenticate('facebook', {
+
+  successRedirect: "/profile",
+  failureRedirect: "/failed"
 
 }));
 
-app.get('/profile',(req,res)=>{
+app.get('/profile', (req, res) => {
 
   res.send("You are a valid user");
 
 })
-app.get('/failed',(req,res)=>{
+app.get('/failed', (req, res) => {
 
   res.send("You are not valid user");
 
 });
 
-passport.serializeUser(function(user,done){
-  done(null,user)
+passport.serializeUser(function (user, done) {
+  done(null, user)
 });
 
-passport.deserializeUser(function(id,done){
-  return done(null,id);
+passport.deserializeUser(function (id, done) {
+  return done(null, id);
 })
 
-app.listen(3000);
+app.listen(3000); */
+
+
+import express from "express"
+import session from "express-session"
+import passport from "passport"
+import passportFacebook from 'passport-facebook';
+
+var FacebookStrategy = passportFacebook.Strategy;
+const app = express()
+
+const port = 3000
+app.use(session({ name: "haiderid", secret: 'abc' }));
+app.use(passport.session());
+
+passport.use(new FacebookStrategy({
+  /* clientID: 367361995459681,
+  clientSecret: "582fed76cff6453955d07557393e8ff5",
+  callbackURL: "http://localhost:3000/auth/facebook/callback" */
+  clientID: 1167069413854243,
+  clientSecret: '4bd8e54032da0c32afd904220a99bae6',
+  callbackURL: "http://localhost:3000/auth/facebook/callback",
+  profileFields: ['id', 'displayName', 'name', 'gender']
+},
+  function (accessToken, refreshToken, profile, done) {
+    console.log(profile)
+    done(null, {profile: profile})
+
+  }
+));
+app.get('/auth/facebook',
+  passport.authenticate('facebook'));
+
+app.get('/auth/facebook/callback',
+  passport.authenticate('facebook', { scope: ['email'] }),
+  function (req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/');
+  });
+app.listen(port)
