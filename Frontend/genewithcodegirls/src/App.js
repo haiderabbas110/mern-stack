0d839c27cea0f5128f1ec06b8b1129d7f9c16c70
= <haidersayani95@gmail.com>
2022-06-14 04:03:50 +0500

    reactjs

---
+++ Frontend/genewithcodegirls/src/App.js
@@ -0,0 +1,12 @@
+import Sidebar from './components/Partial/Sidebar/Sidebar'
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <Sidebar />
+      </header>
+    </div>
+  );
+}
+
+export default App;
