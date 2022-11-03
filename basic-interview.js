33139711ada7e2b56bd5fd76d26f4bf9140e685a
haiderabbas110 <94956683+haiderabbas110@users.noreply.github.com>
2022-11-04 01:55:09 +0500

    interview preparation

---
+++ basic-interview.js
@@ -0,0 +1,12 @@
+Hoisting
+Closure
+type of scopes
+Typed or interface
+Tuples
+debounce
+Throttle
+
+//- Utility function: that does not have any side effect means that do not have some effect anywhere
+let utility = (x) => {
+  return Math.pow(x,3);
+}
