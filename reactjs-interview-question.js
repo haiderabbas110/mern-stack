2fae8904f61329385ed33dbd618fbeafcdf090db
haiderabbas110 <94956683+haiderabbas110@users.noreply.github.com>
2022-11-10 08:12:01 +0500

    Create reactjs-interview-question.js

---
+++ reactjs-interview-question.js
@@ -0,0 +1,8 @@
+/*
+  useMemo
+  useCallback
+*/
+
+//Both Are same - and work for memorized values and do not render again once component called
+//Usememo is used when you have to work with data value = Memoaized value
+//Usememo is used when you have to work with data and whole function or child component Memoaized Funciion
