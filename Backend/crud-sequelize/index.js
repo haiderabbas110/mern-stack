dd71f5a0904438523b3ba1b091f0bc3511fe8e77
= <haidersayani95@gmail.com>
2024-02-23 01:21:30 +0500

    sequilzer crud

--- Backend/crud-sequelize/index.js
+++ Backend/crud-sequelize/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 const app = express();
 const port = 8000;
+app.use(express.urlencoded());
 import { Sequelize, DataTypes } from 'sequelize';
 const sequelize = new Sequelize('bookdb', 'root', '', {
   host: 'localhost',
@@ -15,11 +16,11 @@ const Books = sequelize.define('Books', {
   // id:DataTypes.INTEGER,
   name: DataTypes.STRING,
   author: DataTypes.DATE,
-});
+}, { timestamps: false });
 
 
 
-app.get('/test',async (req, res) => {
+app.get('/test', async (req, res) => {
   try {
     await sequelize.authenticate();
     console.log('Connection has been established successfully.');
@@ -28,13 +29,13 @@ app.get('/test',async (req, res) => {
   }
 })
 
-app.get('/create-table',async (req, res) => {
+app.get('/create-table', async (req, res) => {
   queryInterface.createTable('Books', {
     id: {
       type: Sequelize.INTEGER,
       autoIncrement: true,
       primaryKey: true
-  },
+    },
     name: DataTypes.STRING,
     author: {
       type: DataTypes.BOOLEAN,
@@ -42,18 +43,40 @@ app.get('/create-table',async (req, res) => {
       allowNull: false
     }
   });
- 
+
   res.send("Table Created");
 })
-app.get('/create-book',async (req, res) => {
+app.get('/create-book', async (req, res) => {
   const item = await Books.create({
     name: 'Who is who',
     author: "janedoe",
   });
-  
+
   const books = await Books.findAll();
+  // console.log(books)
+  res.send(books);
+})
+
+app.post("/get-post-by-id/:id", async (req, res) => {
+  console.log(req.params.id);
+  const books = await Books.findOne({
+    where: { id: req.params.id },
+
+  });
+  res.send(books)
+})
+
+
+app.post("/delete-by-id/:id", async (req, res) => {
+  console.log(req.params.id);
+  const books = await Books.destroy({
+    where: {
+      id: req.params.id
+    },
+  });
   console.log(books)
 })
+
 
 app.listen(port, function () {
   console.log(`${port} -> This port has been start`);
